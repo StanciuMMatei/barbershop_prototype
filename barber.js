887f0b38b79f731fe3b216c8ee2f9b6f36a85bc7
StanciuMMatei <mateistanciu7@gmail.com>
2026-08-25 18:29:21 +0300

    Refactor burger menu event listeners

--- barber.js
+++ barber.js
@@ -3,14 +3,11 @@ const nav = document.querySelector(".nav-links");
 const navLinks = document.querySelectorAll(".nav-links li");
 
 burger.addEventListener("click", () => {
-  // Deschide/Inchide meniul
   nav.classList.toggle("nav-active");
 
-  // Animație pentru butonul burger (transformare in X)
   burger.classList.toggle("toggle");
 });
 
-// Inchide meniul automat dupa ce dai click pe un link (pe mobil)
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     if (nav.classList.contains("nav-active")) {
